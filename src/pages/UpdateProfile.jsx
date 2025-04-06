422a646376b040f83e2379d683e7144eacb4acbb
Sayak Banerjee <69157316+gits-up@users.noreply.github.com>
2025-04-06 13:25:36 +0530

    Return button fix

--- src/pages/UpdateProfile.jsx
+++ src/pages/UpdateProfile.jsx
@@ -68,24 +68,6 @@ const UpdateProfile = () => {
 
   return (
     <div className="h-screen w-screen flex items-center justify-center bg-gray-900 overflow-hidden">
-      <button
-        onClick={handleGoBack}
-        className="mb-4 flex items-center text-gray-300 hover:text-white transition-colors"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5 mr-1"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fillRule="evenodd"
-            d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z"
-            clipRule="evenodd"
-          />
-        </svg>
-        Back
-      </button>
       <div className="bg-gray-700 p-6 rounded-xl shadow-lg w-full max-w-lg">
         <h2 className="text-white text-lg font-semibold mb-4 text-center">
           Update Profile
@@ -163,6 +145,12 @@ const UpdateProfile = () => {
               className="w-full bg-blue-600 hover:bg-blue-700 text-white p-2 rounded"
             >
               {uploading ? "Updating..." : "Update Profile"}
+            </button>
+            <button
+              onClick={handleGoBack}
+              className="mb-4 flex items-center text-gray-300 hover:text-white transition-colors"
+            >
+              Return
             </button>
           </div>
         </form>
